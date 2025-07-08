54432869bb6db919f354ce95e4820e3265b95f79
DUVVUSAISIDDARDHA <ds8529@srmist.edu.in>
2025-07-08 12:11:10 +0530

    calculating dialy wage for employee

--- employee.py
+++ employee.py
@@ -1,11 +1,28 @@
 import random
 
 class Employee:
-    def __init__(self,emp_name):
+
+    def __init__(self,emp_id,emp_name,work_time,wage_per_hour):
+        self.emp_id = emp_id
         self.emp_name = emp_name
+        self.work_time = work_time
+        self.wage_per_hour = wage_per_hour
+
     def attendance_check(self):
         attendance = random.randint(0,1)
         if attendance == 0:
-           print(f"{self.emp_name} is Absent")
+           return f"Absent"
         else:
-           print(f"{self.emp_name} is Present")           
+           return f"Present"
+
+    def calculate_dialywage(self):
+        if self.attendance_check():
+            dialywages = self.wage_per_hour * self.work_time
+            return f"employee id :{self.emp_id}\n{self.emp_name} is Present\nDialywage for {self.emp_name} is {dialywages}"
+        else:
+            return f"employee id :{self.emp_id}\n{self.emp_name} is Absent ,No wage is generated "
+
+         
+
+
+        
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from employee import Employee
 
-employee1 = Employee("sai")
+employee1 = Employee("id_1","sai",12,25)
 # USE CASE 1 - attendance checking
 employee1.attendance_check()
+
+# USE CASE 2 - Dialy wage
+print(employee1.calculate_dialywage())
