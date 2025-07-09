52d0e0bcd435766dc83c09cfc19495fc0d93487a
DUVVUSAISIDDARDHA <ds8529@srmist.edu.in>
2025-07-09 10:49:34 +0530

    calculating monthly wages for multiple employee in multiple company

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from company import Company
+from multiple_company import MultipleCompany
 
-# USE CASE 7 -
-my_company = Company("TCS", wage_per_hour=25, max_days=20, max_hours=100)
+multi = MultipleCompany()
 
+multi.add_company("TCS", 25, 20, 100)
+multi.add_company("Infosys", 30, 20, 100)
 
-my_company.add_employee("id_1", "sai", work_time=9)
-my_company.add_employee("id_2", "ram", work_time=8)
+tcs = multi.get_company_by_name("TCS")
+tcs.add_employee("id_1", "Sai", 9)
+tcs.add_employee("id_2", "Kiran", 8)
 
+infosys = multi.get_company_by_name("Infosys")
+infosys.add_employee("id_1", "Raj", 7)
+infosys.add_employee("id_2", "Meena", 9)
 
-my_company.calculate_all_employee_monthly_wage()
+multi.calculate_all_wages()
